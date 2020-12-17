216b93b2f1f64650a494b590fa7289d0a39c7ec0
= <=>
2020-12-16 21:06:57 -0500

    fix mutations ref

--- quasar/src/graphql/index.js
+++ quasar/src/graphql/index.js
@@ -1,9 +1,9 @@
 export * as accountTypeDefs from './Accounts/typeDefs'
 export * as accountQueries from './Accounts/queries'
 export { resolvers as accountResolvers} from './Accounts/resolvers'
-export * as accountMutations from './Accounts/queries'
+export * as accountMutations from './Accounts/mutations'
 
 export * as moduleTypeDefs from './Modules/typeDefs'
 export * as moduleQueries from './Modules/queries'
 export { resolvers as moduleResolvers} from './Modules/resolvers'
-export * as moduleMutations from './Modules/queries'
+export * as moduleMutations from './Modules/mutations'
--- quasar/src/pages/Settings.vue
+++ quasar/src/pages/Settings.vue
@@ -16,7 +16,6 @@
 </template>
 
 <script>
-
 import {
   accountQueries,
   moduleQueries,
